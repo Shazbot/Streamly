b50f64a987ce61a63fb4f5bc751a34b9729d2f10
tilic <tilic5@gmail.com>
2014-06-15 17:24:13 +0200

    trying to get fluent assertions to work

--- LeStreamsFace.Tests/IsJSONParseSameAsXMLParse.cs
+++ LeStreamsFace.Tests/IsJSONParseSameAsXMLParse.cs
@@ -2,10 +2,13 @@
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using System.Threading.Tasks;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using FluentAssertions;
 using LeStreamsFace.StreamParsers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
 using Xunit;
 
 namespace LeStreamsFace.Tests
@@ -34,6 +37,26 @@ namespace LeStreamsFace.Tests
             var streams = parseJSON.GetStreamsFromContent(input);
 
             Approvals.VerifyAll(streams, "");
+        }
+
+        [Fact]
+        public async void DoXMLandJSONparseGiveTheSameResult()
+        {
+            var XMLparser = new TwitchXMLStreamParser();
+            var twitchXMLResponse = new RestClient("http://api.justin.tv/api/stream/list.xml?category=gaming&limit=100").ExecuteTaskAsync(new RestRequest());
+
+            var JSONparser = new TwitchJSONStreamParser();
+            var twitchJSONResponse = new RestClient("https://api.twitch.tv/kraken/streams?limit=100").SinglePageResponse();
+            var streamsFromJSON = JSONparser.GetStreamsFromContent(twitchJSONResponse.Content);
+
+            var streamsFromXML = XMLparser.GetStreamsFromContent((await twitchXMLResponse).Content);
+            streamsFromXML = streamsFromXML.Skip(1);
+
+//            streamsFromJSON.Count().ShouldBe(streamsFromXML.Count()-1);
+//            streamsFromJSON.ShouldBe(streamsFromXML);
+
+//            (new[] { 1, 2, 3 }).ShouldBe(new[] { 1, 2, 4 });
+            (new[] { 1, 2, 3 }).Should().BeSameAs(new[] { 1, 2, 4 });
         }
     }
 }
