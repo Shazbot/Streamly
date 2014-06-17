bb52bd3f40776567ae94f47787e7c1512699eddc
tilic <tilic5@gmail.com>
2014-06-17 02:28:07 +0200

    messing with cef

--- LeStreamsFace/CefWebView.xaml.cs
+++ LeStreamsFace/CefWebView.xaml.cs
@@ -156,8 +156,8 @@ namespace LeStreamsFace
         {
             webBrowser.Address = InputText;
 
-            var wings = @"<object type=""application/x-shockwave-flash"" height=""378"" width=""620"" id=""live_embed_player_flash"" data=""http://www.twitch.tv/widgets/live_embed_player.swf?channel=wingsofdeath"" bgcolor=""#000000""><param name=""allowFullScreen"" value=""true"" /><param name=""allowScriptAccess"" value=""always"" /><param name=""allowNetworking"" value=""all"" /><param name=""movie"" value=""http://www.twitch.tv/widgets/live_embed_player.swf"" /><param name=""flashvars"" value=""hostname=www.twitch.tv&channel=wingsofdeath&auto_play=true&start_volume=25"" /></object><a href=""http://www.twitch.tv/wingsofdeath"" style=""padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline; text-align:center;"">Watch live video from Wingsofdeath on www.twitch.tv</a>";
-            webView.LoadHtml(wings, wings);// = wings;
+            var wings = @"<object type=""application/x-shockwave-flash"" height="""+webView.Height+@""" width="""+webView.Width+@""" id=""live_embed_player_flash"" data=""http://www.twitch.tv/widgets/live_embed_player.swf?channel=wingsofdeath"" bgcolor=""#000000""><param name=""allowFullScreen"" value=""true"" /><param name=""allowScriptAccess"" value=""always"" /><param name=""allowNetworking"" value=""all"" /><param name=""movie"" value=""http://www.twitch.tv/widgets/live_embed_player.swf"" /><param name=""flashvars"" value=""hostname=www.twitch.tv&channel=wingsofdeath&auto_play=true&start_volume=25"" /></object><a href=""http://www.twitch.tv/wingsofdeath"" style=""padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline; text-align:center;"">Watch live video from Wingsofdeath on www.twitch.tv</a>";
+            webView.LoadHtml(wings, "arst");// = wings;
         }
 
         public string InputText
